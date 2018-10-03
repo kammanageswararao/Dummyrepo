a43ad99e00c3a88e8091dc602a6b9819f4de3877
kamma nageswararao <kamma.nageswararao@gmail.com>
2018-10-03 15:29:08 +0530

    First Committ

---
+++ Sample/src/SampleClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author knageswarar2
+ *
+ */
+public class SampleClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("code move to GIT");
+
+	}
+
+}
